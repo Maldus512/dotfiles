de519e863d00021d239ffba51f00f146bb1841db
Maldus512 <mattia512maldini@gmail.com>
2023-11-23 06:17:07 +0100

    dap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -23,6 +23,7 @@ vim.keymap.set('n', '<F11>', function() dap.step_into() end)
 vim.keymap.set('n', '<F12>', function() dap.step_out() end)
 vim.keymap.set('n', '<Leader>b', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>B', function() dap.set_breakpoint() end)
+--[[
 --vim.keymap.set('n', '<Leader>lp', function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 vim.keymap.set('n', '<Leader>dr', function() dap.repl.open() end)
 vim.keymap.set('n', '<Leader>dl', function() dap.run_last() end)
@@ -40,6 +41,7 @@ vim.keymap.set('n', '<Leader>ds', function()
     local widgets = require('dap.ui.widgets')
     widgets.centered_float(widgets.scopes)
 end)
+--]]
 
 --[[
 --Spectre
